810b278a0440b12dc332625e9b164f3662aa025a
AlanBR72 <50175645+AlanBR72@users.noreply.github.com>
2026-03-09 12:51:31 -0300

    requirements.txt
    requests
    beautifulsoup4

---
+++ bot_rucoy.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+
+url = "https://www.rucoyonline.com/characters/Bank%20Of%20Alan"
+webhook = "https://discord.com/api/webhooks/1391526244511907900/X4qA3HTFE9NLO2klBMO3LzxvrLQKoQZWEPITkYWY5f0ws9PmREUp4mXa9F1kCvROKEv8"
+
+ultimo_status = None
+hora_login = None
+
+def enviar(msg):
+    requests.post(webhook, json={"content": msg})
+
+def verificar_status():
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    texto = soup.text.lower()
+
+    if "currently online" in texto:
+        return "online"
+    else:
+        return "offline"
+
+try:
+
+    status = verificar_status()
+    ultimo_status = status
+
+    agora = datetime.now().strftime("%H:%M:%S")
+
+    emoji = "🟢" if status == "online" else "🔴"
+
+    mensagem_inicio = (
+        "🚀 **Rucoy Tracker iniciado**\n\n"
+        "👤 Personagem: **Bank Of Alan**\n"
+        f"📡 Status atual: **{emoji} {status.upper()}**\n"
+        "⏱ Verificação: **1 minuto**"
+    )
+
+    enviar(mensagem_inicio)
+
+    if status == "online":
+        hora_login = datetime.now()
+
+    while True:
+        agora = datetime.now().strftime("%H:%M:%S")
+        print(f"[{agora}] Verificando perfil...")
+
+        status = verificar_status()
+        print("Status:", status)
+
+        if status != ultimo_status:
+
+            hora_atual = datetime.now()
+
+            if status == "online":
+                hora_login = hora_atual
+                enviar(f"🟢 Bank Of Alan logou às {hora_atual.strftime('%H:%M:%S')}")
+
+            if status == "offline":
+                if hora_login:
+                    tempo = hora_atual - hora_login
+                    horas = tempo.seconds // 3600
+                    minutos = (tempo.seconds % 3600) // 60
+
+                    enviar(
+                        f"🔴 Bank Of Alan deslogou às {hora_atual.strftime('%H:%M:%S')}\n"
+                        f"⏱ Tempo online: {horas}h {minutos}m"
+                    )
+
+            ultimo_status = status
+
+        time.sleep(60)
+
+except KeyboardInterrupt:
+
+    enviar("🛑 Bot de monitoramento finalizado")
+    print("Bot encerrado.")
